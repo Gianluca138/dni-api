2448772257286b60699a131a73553cdea9ce58b9
Gianluca138 <bartolomeogianluca02@gmail.com>
2026-07-13 18:18:13 -0300

    pls

--- utils/generateDNI.js
+++ utils/generateDNI.js
@@ -35,7 +35,7 @@ const POS = {
         // Centrado debajo de "Apellido/Surname"
         apellido: {
             x: 560, // Ajustado al centro del espacio de texto
-            y: 270 
+            y: 295 
         },
 
         // Centrado debajo de "Nombre/Name"
